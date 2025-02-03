c82a9bf79612da3eef12f7bb58d0b79f2eb33a01
Michał Dacko <dackodev@gmail.com>
2025-02-03 23:14:48 +0100

    Add auth actions

---
+++ src/actions/signupAction.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+import { signupSchema } from '@/lib/schemas/signupSchema'
+import { TActionResponse } from '@/types'
+
+export async function signupAction(formData: FormData) {
+    const supabase = await createClient()
+
+    const parsed = signupSchema.safeParse(
+        Object.fromEntries(formData.entries())
+    )
+
+    if (!parsed.success) {
+        return {
+            isError: true,
+            message: parsed.error.errors[0].message,
+        } as TActionResponse
+    }
+
+    const { error } = await supabase.auth.signUp(parsed.data)
+
+    if (error) {
+        console.error('Error signing up:', error.message)
+        return {
+            isError: true,
+            message: 'Something went wrong. Please try again later.',
+        } as TActionResponse
+    }
+
+    revalidatePath('/', 'layout')
+    redirect('/')
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type TActionResponse = {
+    data?: unknown
+    isError?: boolean
+    message?: string
+}
